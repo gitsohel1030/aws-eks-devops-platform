d2019c90e4cea1f2731f01d8f8b0316adf7dffe3
sohel mujawar <sohelmujawar172@gmail.com>
2026-05-02 10:41:30 +0000

    add: created OIDC and role for github actions

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -34,6 +34,87 @@ module "argocd" {
   ]
 }
 
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  tags = {
+    Environment = "prod"
+    Terraform = "true"
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "${var.cluster_name}-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:gitsohel1030/foodrush:*"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Environment = "prod"
+    Terraform = "true"
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions" {
+  name = "${var.cluster_name}-github-actions-policy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # ECR login
+      {
+        Effect = "Allow"
+        Action = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      # ECR push/pull for foddrush repos
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:DescribeImages",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = [
+          "arn:aws:ecr:${var.region}:${data.aws_caller_identity.current.account_id}:repository/foodrush-*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = "eks:DescribeCluster"
+        Resource = "arn:aws:eks:${var.region}:${data.aws_caller_identity.current.account_id}:cluster/${var.cluster_name}"
+      }
+    ]
+  })
+}
+
 
 
 # module "argocd" {
--- terraform/envs/prod/outputs.tf
+++ terraform/envs/prod/outputs.tf
@@ -6,3 +6,6 @@ output "loki_irsa_role_arn" {
   value = module.loki_irsa_role.iam_role_arn
 }
  
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
